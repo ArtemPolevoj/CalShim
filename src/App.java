3fa236ce27b74212333c7c0d7b139ab21b0ee5dc
Артём <artempolevoj@yandex.ru>
2023-06-12 19:30:11 +0300

    Версия 1

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends javafx.application.Application{
+    public static void run() {
+        launch();
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 380);
+        stage.setTitle("Калькулятор шимов");
+        stage.getIcons().add(new Image("tr.png"));
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends javafx.application.Application{
+public class Main {
     public static void main(String[] args) {
-        launch();
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 300, 380);
-        stage.setTitle("Калькулятор шимов");
-        stage.setResizable(false);
-        stage.setScene(scene);
-        stage.show();
+        App.run();
     }
 }

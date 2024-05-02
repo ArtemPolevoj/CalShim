7ff4921bb30749ac22a279095755488f2bee2ce7
artem <artempolevoj@yandex.ru>
2024-05-02 21:08:44 +0300

    add README

--- src/ModText.java
+++ src/ModText.java
@@ -6,7 +6,7 @@ import java.util.TreeMap;
 public class ModText {
     public static final Map<Double, Integer> mapResult = new TreeMap<>();
 
-    public static String getText(String inputShimValue){
+    public static String getText(String inputShimValue) {
         mapResult.clear();
         List<Double> list = ModList.getList();
         Collections.sort(list);
@@ -17,34 +17,30 @@ public class ModText {
         for (Double i : list) {
             max = Math.nextDown(max) + Math.nextDown(i);
         }
-        if (list.isEmpty()){
+        if (list.isEmpty()) {
             return "Проверь введённые значения.";
         }
-        if (dobInputShimValue <= 0){
-            return  "Проверь искомое значение.";
+        if (dobInputShimValue <= 0) {
+            return "Проверь искомое значение.";
         }
-        if (dobInputShimValue > max){
+        if (dobInputShimValue > max) {
             outText = new StringBuilder("Не хватает шимов");
         } else if (dobInputShimValue == max
-                || (dobInputShimValue - list.get(0)/2) == max) {
+                || (dobInputShimValue - list.getFirst() / 2) == max) {
             outText = new StringBuilder("Возьмите весь пакет целиком.\n" +
                     "Размер пакета шимов - " + max);
-        } else if ((dobInputShimValue - list.get(0)/2) <= 0) {
+        } else if ((dobInputShimValue - list.getFirst() / 2) <= 0) {
             outText = new StringBuilder("Не используйте шимы.");
         } else {
             calShim(list, dobInputShimValue);
-            for (Double k: mapResult.keySet()) {
+            for (Double k : mapResult.keySet()) {
                 sizeShimPak = sizeShimPak + k * mapResult.get(k);
                 outText.append("Шим ").append(k).append(" мм. - ")
                         .append(mapResult.get(k)).append(" шт.\n");
             }
             outText.append(String.format("Размер пакета шимов - %.2f мм.", sizeShimPak));
-
         }
-
-
         return outText.toString();
-
     }
 
     private static void calShim(List<Double> array, double value) {
@@ -61,11 +57,10 @@ public class ModText {
                 mapResult.put(array.get(i), count);
             }
         }
-        if ((value - sum) >= (array.get(0) / 2)) {
-            if (mapResult.containsKey(array.get(0))) mapResult.put(array.get(0),
-                    mapResult.get(array.get(0)) + 1);
-            else  mapResult.put(array.get(0), 1);
+        if ((value - sum) >= (array.getFirst() / 2)) {
+            if (mapResult.containsKey(array.getFirst())) mapResult.put(array.getFirst(),
+                    mapResult.get(array.getFirst()) + 1);
+            else mapResult.put(array.getFirst(), 1);
         }
     }
-
 }
